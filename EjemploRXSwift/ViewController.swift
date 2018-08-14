bba47d9f1bc3a2c6a1ab81e979fddb9b8ff7a95b
Marcos Suarez <msuarez@iphonedroid.com>
2018-08-14 12:44:36 +0200

    Se creo un observable para el splider y la etiqueta del slider

--- EjemploRXSwift/ViewController.swift
+++ EjemploRXSwift/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPresionar: UIButton!
+    
+    @IBOutlet weak var labelBtnPresionar: UILabel!
+    
+    @IBOutlet weak var swicth: UISwitch!
+    
+    @IBOutlet weak var labelSwitch: UILabel!
+    
+    @IBOutlet weak var labelSlider: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    // Control para evitar ciclos de retención
+    // Todas la variables Rx deben estar registradas aquí.
+    let disposeBag = DisposeBag()
+    
+    
+    var monitor = ["inicio", "llegando al medio", "medio", "llegando al fin","fin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupObservables()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupObservables() {
+        
+        let observarSlider = slider.rx.value
+            .asObservable()
+            .share()
+        
+        observarSlider
+            .map{ Int($0) }
+            .map({ (valor) -> String in
+                return valor.description
+            })
+            .bind(to: labelSlider.rx.text)
+            .disposed(by: disposeBag)
+        
+        
+        
+        
+        
     }
-
 
 }
 
