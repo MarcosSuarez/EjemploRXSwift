c539f19ee140664aba4bb538f336766f35ee0c23
Marcos Suarez <msuarez@iphonedroid.com>
2018-08-14 11:07:38 +0200

    Initial Commit

---
+++ EjemploRXSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploRXSwift
+//
+//  Created by Marcos Suarez on 14/8/18.
+//  Copyright © 2018 Marcos Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
